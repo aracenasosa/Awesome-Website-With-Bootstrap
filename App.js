bcdb594a982a011d7ce5f710b27b325c2114a2e3
miusuario <carlosthefather2014@hotmail.com>
2019-03-29 23:03:25 -0400

    Primer Commit

---
+++ App.js
@@ -0,0 +1,46 @@
+(function(){
+    
+    window.sr = ScrollReveal();
+
+    sr.reveal('.navbar',{
+        duration: 3000,
+        origin: 'bottom'
+    });
+
+    sr.reveal('.header-content-left',{
+        duration: 4000,
+        origin: 'top',
+        distance: '300px'
+    });
+
+    sr.reveal('.header-content-right',{
+        duration: 3000,
+        origin: 'right',
+        distance: '300px'
+    });
+
+    sr.reveal('.header-btn',{
+        duration: 3000,
+        origin: 'bottom',
+        delay: 1500 
+    });
+
+    sr.reveal('#testimonial',{
+        duration: 5000,
+        origin: 'left',
+        distance: '300px',
+        viewFactor: 0.2 
+    });
+
+    //Smoth Scrolling
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+    
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+}());
